c6e9c654c11e0ed1fa03a8c46654620c54923fdb
Kyle <kylebennett87@gmail.com>
2019-11-26 20:58:39 +0000

    Initial project setup

---
+++ src/main/java/com/kylebennett/dwpdigitaltechtest/DwpDigitalTechTestApplication.java
@@ -0,0 +1,13 @@
+package com.kylebennett.dwpdigitaltechtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwpDigitalTechTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwpDigitalTechTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kylebennett/dwpdigitaltechtest/DwpDigitalTechTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kylebennett.dwpdigitaltechtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwpDigitalTechTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
